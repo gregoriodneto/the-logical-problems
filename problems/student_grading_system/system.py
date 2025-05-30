7629d4bbd73caa7a88a26be5dd633149b539dcd7
gregoriodneto <igregoriodoneto01@gmail.com>
2025-05-30 08:10:06 -0300

    feat: Percentual de Aprovados e Reprovados

--- problems/student_grading_system/system.py
+++ problems/student_grading_system/system.py
@@ -2,15 +2,25 @@ import os, json
 
 def notes_students(notes):
     grouping = {}
+    approved = 0
+    failed = 0
     for note in notes:
         student = note["name"]
         average = round(sum(note["grades"]) / len(note["grades"]), 2)
-        attendance = note["attendance"]
+        attendance = note["attendance"]        
         grouping[student] = {
             "average": average,
             "attendance": attendance,
             "status": "Approved" if average >= 7 and attendance >= 75 else "Failed"
         }
+        approved += 1 if grouping[student]["status"] == "Approved" else 0
+        failed += 1 if grouping[student]["status"] == "Failed" else 0
+    grouping["summary"] = {
+        "approved": approved,
+        "failed": failed,
+        "approved_percent": round((approved * 100) / len(notes), 2),
+        "failed_percent": round((failed * 100) / len(notes),2)
+    }
     return grouping
 
 if __name__ == "__main__":
